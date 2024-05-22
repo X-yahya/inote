ecc7c3a819c5d71e87b85458d853224b3596f1f2
lyt <124516380+X-yahya@users.noreply.github.com>
2024-05-22 17:06:40 +0100

    update

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
   <div className="absolute top-1/2 left-1/2 text-white -translate-x-1/2 -translate-y-1/2">
     <h1 className="font-semibold text-7x1 text-center tracking-wider	">
       i<span className="text-blue-500 font-bold">note</span></h1>
-  <div className="mt-4"></div>
+  <div className="mt-4">
+    <img src="./src/assets/curve.png" alt="" />
+  </div>
   <h2 className="font-semibold text-3x1 text-center text-white">
     Ai Powered
   </h2>
